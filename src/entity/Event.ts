9c8dff8a4967179cadd59470765972172c8b2238
hsingyuc <hsingyuc.7@gmail.com>
2021-10-05 16:09:29 -0400

    Create event entity

---
+++ src/entity/Event.ts
@@ -0,0 +1,32 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from "typeorm";
+import { Receiver } from "./Receiver";
+
+@Entity()
+export class Event {
+
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({ unique: true })
+	date: number;
+
+	@Column()
+	latitude: number;
+
+	@Column()
+	longitude: number;
+
+	@Column()
+	duration: string;
+
+	@Column()
+	status: string;
+
+	@Column()
+	note: string;
+
+	@ManyToMany(() => Receiver)
+	@JoinTable()
+	receivers: Receiver[];
+
+}
