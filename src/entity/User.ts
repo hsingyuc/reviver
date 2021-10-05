7a3e98291e37921d5cdf5a4a254ac183a2821132
hsingyuc <hsingyuc.7@gmail.com>
2021-10-05 14:22:05 -0400

    Create user entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 
 @Entity()
 export class User {
@@ -7,12 +7,21 @@ export class User {
     id: number;
 
     @Column()
+    accountName: string;
+
+    @Column()
+    password: string;
+
+    @Column()
     firstName: string;
 
     @Column()
     lastName: string;
 
     @Column()
-    age: number;
+    email: number;
+
+    @Column()
+    phone: number;
 
 }
