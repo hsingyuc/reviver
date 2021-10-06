f627f37afe21a6d319780ae398b0aa1ed6b8b7c7
hsingyuc <hsingyuc.7@gmail.com>
2021-10-06 18:45:29 -0400

    Remove get all users

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,14 +1,10 @@
-import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
-import {User} from "../entity/User";
+import { getRepository } from "typeorm";
+import { NextFunction, Request, Response } from "express";
+import { User } from "../entity/User";
 
 export class UserController {
 
     private userRepository = getRepository(User);
-
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
-    }
 
     async one(request: Request, response: Response, next: NextFunction) {
         return this.userRepository.findOne(request.params.id);
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,6 @@
-import {UserController} from "./controller/UserController";
+import { UserController } from "./controller/UserController";
 
 export const Routes = [{
-    method: "get",
-    route: "/users",
-    controller: UserController,
-    action: "all"
-}, {
     method: "get",
     route: "/users/:id",
     controller: UserController,
