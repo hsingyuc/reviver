a5c782f6dfaa028ad7f73a2ab1e3c34091b11f33
Hsing-yu Flowers <hsingyuc.7@gmail.com>
2021-10-14 21:01:42 -0400

    Remove get all receivers; (#35)

--- src/controller/ReceiverController.ts
+++ src/controller/ReceiverController.ts
@@ -6,16 +6,6 @@ import { validate } from 'class-validator';
 export class ReceiverController {
 	private receiverRepository = getRepository(Receiver);
 
-	async all(request: Request, response: Response, next: NextFunction) {
-		const foundAll = await this.receiverRepository.find();
-
-		if (!foundAll) {
-			response.status(404);
-			return { message: 'Unable to find the requested receivers.' };
-		}
-		return foundAll;
-	}
-
 	async one(request: Request, response: Response, next: NextFunction) {
 		const foundOne = await this.receiverRepository.findOne(request.params.id);
 
