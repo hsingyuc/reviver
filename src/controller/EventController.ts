6a624c31da4283cba7fbcc5e3aba5fd3fedc535f
hsingyuc <hsingyuc.7@gmail.com>
2021-10-19 22:29:26 -0400

    Add create event method

--- src/controller/EventController.ts
+++ src/controller/EventController.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { Event } from "../entity/Event";
 import { NextFunction, Request, Response } from 'express';
+import { validate } from 'class-validator';
 
 export class EventController {
 	private eventRepository = getRepository(Event);
@@ -23,5 +24,22 @@ export class EventController {
 			return { message: 'Unable to find the requested event.' };
 		}
 		return foundOne;
+	}
+
+	async create(request: Request, response: Response, next: NextFunction) {
+		const eventCreated = this.eventRepository.create(request.body);
+		const errors = await validate(eventCreated);
+
+		if (errors.length > 0) {
+			response.status(422);
+			return { message: 'Please provide valid input fields.', errors };
+		}
+		try {
+			await this.eventRepository.save(eventCreated);
+			return { message: 'Event created.', event: eventCreated };
+		} catch (error) {
+			response.status(500);
+			return { message: 'Internal Server Error.' };
+		}
 	}
 }
