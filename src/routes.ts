d63075dce8a34e55ceb5898912cc29aa6c6efe28
hsingyuc <hsingyuc.7@gmail.com>
2021-10-21 11:53:49 -0400

    Add update event route

--- src/routes.ts
+++ src/routes.ts
@@ -57,4 +57,9 @@ export const Routes = [{
     route: "/events/:id",
     controller: EventController,
     action: "remove"
+}, {
+    method: "put",
+    route: "/events/:id",
+    controller: EventController,
+    action: "update"
 }];
