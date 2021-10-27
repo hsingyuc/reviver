73857d12d2320086dd23e9486744b494e8039812
Hsing-yu Flowers <hsingyuc.7@gmail.com>
2021-10-26 23:25:38 -0400

    Add route middleware method (#81)

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,26 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { Routes } from "./routes";
+import * as cookieParser from 'cookie-parser';
 
 createConnection().then(async connection => {
 
     // create express app
     const app = express();
+
     app.use(bodyParser.json());
+    app.use(cookieParser());
 
     // register express routes from defined application routes
     Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+        const middleware = route.middleware
+            ? route.middleware
+            : ( req: Request, res: Response, next: Function ) => next();
+
+        (app as any)[route.method](
+            route.route, 
+            middleware,
+            (req: Request, res: Response, next: Function) => {
             const result = (new (route.controller as any))[route.action](req, res, next);
             if (result instanceof Promise) {
                 result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
