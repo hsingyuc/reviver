096dacbccda7969a097e265c0fc3e28b5e5f4137
hsingyuc <hsingyuc.7@gmail.com>
2021-11-04 22:57:52 -0400

    Remove create user route

--- src/routes.ts
+++ src/routes.ts
@@ -4,11 +4,6 @@ import { EventController } from "./controller/EventController";
 import { AuthController } from "./controller/AuthController";
 
 export const Routes = [{
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "create"
-}, {
     method: "delete",
     route: "/users/:id",
     controller: UserController,
