5e02427569e0f1c061fe924418f98a714624c2c2
hsingyuc <hsingyuc.7@gmail.com>
2021-11-04 22:26:11 -0400

    Verify token

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,13 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../entity/User';
+
+const jwt = require('jsonwebtoken');
 
 export const authenticate = (request: Request, response: Response, next: NextFunction) => {
 	const token = request.cookies.loginToken;
+	const userRepository = getRepository(User);
 
-	// @Todo Get the user if its set(can get userid from the token)
+	if (!token) {
+		response.status(401);
+		return response.json({ message: 'Unauthenticate.' });
+	}
+	jwt.verify(token, 'secret', async function (err, data) {
+		if (err) {
+			response.status(401);
+			return response.json({ message: 'Unauthenticate.' });
+		}
+		const user = await userRepository.findOne(data.id);
 
-	next();
-
-	// @Todo If its not set, then respond forbidden
-}
+		if (!user) {
+			response.status(401);
+			return response.json({ message: 'No user found.' });
+		}
+		request.user = user;
+		return next();
+	})
+};
 
 export default authenticate;
